3110460ff2b2ef8510e8d13698047b13a87bad03
yuzuki <nanami2962@gmail.com>
2021-06-19 14:25:19 +0800

    lab10 finished

--- lab/lab10/lab10.scm
+++ lab/lab10/lab10.scm
@@ -1,43 +1,54 @@
 (define (over-or-under num1 num2)
-  (cond 
-    ((> num1 num2) 1)
-    ((= num1 num2) 0)
-    ((< num1 num2) -1)))
-
-; ;; Tests
-(over-or-under 1 2)
-
-; expect -1
-(over-or-under 2 1)
-
-; expect 1
-(over-or-under 1 1)
-
-; expect 0
-(define (make-adder num) (lambda (x) (+ num x)))
-
-; ;; Tests
-(define adder (make-adder 5))
-
-(adder 8)
-
-; expect 13
-(define (composed f g) (lambda (x) (f (g x))))
-
-(define lst
-
+  (cond (< num1 num2) -1
+        (= num1 num2) 0
+        else 1)
 )
 
-(define (remove item lst) 
-    (filter (lambda (x) (not (= x item))) lst))
+;;; Tests
+(over-or-under 1 2)
+; expect -1
+(over-or-under 2 1)
+; expect 1
+(over-or-under 1 1)
+; expect 0
 
-; ;; Tests
+
+(define (make-adder num)
+  (lambda (inc) (+ num inc))
+)
+
+;;; Tests
+(define adder (make-adder 5))
+(adder 8)
+; expect 13
+
+
+(define (composed f g)
+  (lambda (x) (f (g x)))
+)
+
+
+(define lst
+  (list '((1) 2 (3 4) 5))
+)
+
+
+
+(define (remove item lst)
+  (if (eq? lst nil)
+    '()
+    (if (eq? item (car lst))
+      (remove item (cdr lst))
+      (cons (car lst) (remove item (cdr lst)))
+    ))
+)
+
+
+;;; Tests
 (remove 3 nil)
-
 ; expect ()
 (remove 3 '(1 3 5))
-
 ; expect (1 5)
 (remove 5 '(5 3 5 5 1 4 5 4))
-
 ; expect (3 1 4 4)
+
